052d6c351a12c5c07f346e37b290402996e05b81
liyang <121777824@qq.com>
2017-04-08 15:48:29 +0800

    用户数据分页

--- routers/admin.js
+++ routers/admin.js
@@ -6,8 +6,8 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
-router.use((req,res,next)=>{
-  if(!req.userInfo.isAdmin){
+router.use((req, res, next) => {
+  if (!req.userInfo.isAdmin) {
     res.send('对不起，只有管理员才可以进入后台管理系统')
     return
   }
@@ -15,19 +15,45 @@ router.use((req,res,next)=>{
 })
 
 //首页
-router.get('/',(req,res,next)=>{
-  res.render('admin/index',{
-    userInfo : req.userInfo
+router.get('/', (req, res, next) => {
+  res.render('admin/index', {
+    userInfo: req.userInfo
   })
 })
 
 //用户管理
-router.get('/user',(req,res,next)=>{
-  User.find().then((users)=>{
-    res.render('admin/user_index',{
-      userInfo:req.userInfo,
-      users:users
+//limit(num)：限制获取的数据条数
+//skip(num):忽略数据的条数
+
+
+router.get('/user', (req, res, next) => {
+  let page = Number(req.query.page || 1)
+  const limit = 3
+  let pages = 0
+  //计算总条数
+  User.count().then((count) => {
+
+    //计算总页数
+    pages = Math.ceil(count / limit)
+    //取值不能大于pages
+    page = Math.min( page, pages)
+    //取值不能小于1
+    page = Math.max( page, 1 )
+
+    let skip = (page - 1) * limit
+
+    User.find().limit(limit).skip(skip).then((users) => {
+      res.render('admin/user_index', {
+        userInfo: req.userInfo,
+        users: users,
+        count:count,
+        pages:pages,
+        limit:limit,
+        page: page
+      })
     })
   })
+
+
 })
 module.exports = router
