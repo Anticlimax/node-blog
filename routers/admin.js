57373ba8c1ecbdec349f5d6db6ff3f745ece193f
liyang <121777824@qq.com>
2017-04-08 14:17:30 +0800

    后台初步搭建

--- routers/admin.js
+++ routers/admin.js
@@ -5,8 +5,21 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/user',(req,res,next)=>{
-  res.send('ADMIN - User')
+router.use((req,res,next)=>{
+  if(!req.userInfo.isAdmin){
+    res.send('对不起，只有管理员才可以进入后台管理系统')
+    return
+  }
+  next()
 })
+
+//首页
+router.get('/',(req,res,next)=>{
+  res.render('admin/index',{
+    userInfo : req.userInfo
+  })
+})
+
+//用户管理
 
 module.exports = router
